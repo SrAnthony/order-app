1e79dd2e406c24a9a89ca6fd6668535ad316294b
aluno-uffs <aluno-uffs@405host07.cco.uffs.edu.br>
2019-11-25 16:41:37 -0300

    Login com back - funcionando

--- src/Screens/Login.js
+++ src/Screens/Login.js
@@ -1,7 +1,27 @@
-import React from 'react'
-import { Content, Container, Form, Item, Input, Label, Button, Icon, Text } from 'native-base'
+import React, {useState, useEffect} from 'react'
+import { Content, Container, Form, Item, Input, Label, Button, Icon, Text, Toast } from 'native-base'
+import { API } from '../Utils/endpoints'
 
-export default () => {
+export default ({ navigation }) => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const submit = () => {
+    API.post('/api/Client/login', {email_cpf: email, password: password})
+      .then(response => {
+          if (response.data.result){
+            Toast.show({
+              text: 'Login efetuado!'
+            })
+            navigation.navigate('Orders')
+          }else{
+            Toast.show({
+              text: 'Wrong Password or Email!'
+            })
+          }
+      })    
+      .catch(err => console.log(err))
+  }
 
   return (
     <Container>
@@ -9,17 +29,17 @@ export default () => {
         <Form>
           <Item floatingLabel>
             <Label>Email</Label>
-            <Input />
+            <Input value={email} on onChangeText={setEmail}/>
           </Item>
           <Item floatingLabel last>
             <Label>Senha</Label>
-            <Input secureTextEntry={true} />
+            <Input secureTextEntry={true} value={password} onChangeText={setPassword}/>
           </Item>
         </Form>
 
         <Button block style={{ marginTop: 40 }} success iconLeft>
           <Icon name='people' />
-          <Text>Entrar</Text>
+          <Text  onPress={submit}>Entrar</Text>
         </Button>
         <Button block style={{ marginTop: 10 }} primary>
           <Icon name='paper' />
